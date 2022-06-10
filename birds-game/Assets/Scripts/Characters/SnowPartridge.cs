6d094caf41a1774f28f681310e2c6ac2a5b7d740
Valentyn Andrushko <vandrushko@weecodelab.com>
2022-06-11 01:31:56 +0200

    Added classes with remaining birds with base values

---
+++ birds-game/Assets/Scripts/Characters/SnowPartridge.cs
@@ -0,0 +1,11 @@
+namespace birds_game.Assets.Scripts.Characters
+{
+    public class SnowPartridge : BirdCharacter
+    {
+        public override float WalkingSpeed => 1;
+        public override float JumpPower => 1;
+        public override float TakeOffPower => 1;
+        public override float Mass => 1;
+        public override string Name => "Snow partridge";
+    }
+}
