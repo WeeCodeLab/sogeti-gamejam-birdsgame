e1f21c67c6fa1bf087017c6775f7756662c35355
Олежа Блаженный <MrRavMorgan@gmail.com>
2022-06-12 17:54:11 +0300

    Added HealthUi component

---
+++ birds-game/Assets/Scripts/Ui/HealthContainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace birds_game.Assets.Scripts
+{
+    public class HealthContainer : MonoBehaviour
+    {
+        public bool IsBroken { get; private set; } = false;
+        
+        [SerializeField] private GameObject _full;
+        [SerializeField] private GameObject _empty;
+
+        private void Start()
+        {
+            _empty.SetActive(false);
+        }
+
+        public void FreeContainer()
+        {
+            IsBroken = true;
+            _full.SetActive(false);
+            _empty.SetActive(true);
+        }
+
+        public void FillContainer()
+        {
+            IsBroken = false;
+            _full.SetActive(true);
+            _empty.SetActive(false);
+        }
+    }
+}
---
+++ birds-game/Assets/Scripts/Ui/HealthUi.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace birds_game.Assets.Scripts
+{
+    public class HealthUi : MonoBehaviour
+    {
+        [SerializeField] private HealthContainer[] healthContainers = new HealthContainer[3];
+
+        public void OnTakeDamage()
+        {
+            foreach (HealthContainer feather in healthContainers)
+            {
+                if(!feather.IsBroken)
+                {
+                    feather.FreeContainer();
+                    return;
+                }
+            }
+        }
+
+        public void OnRegenerateHealth()
+        {
+            for (int i = 2; i >= 0; i--)
+            {
+                if(healthContainers[i].IsBroken)
+                {
+                    healthContainers[i].FillContainer();
+                    return;
+                }
+            }
+        }
+    }
+}
