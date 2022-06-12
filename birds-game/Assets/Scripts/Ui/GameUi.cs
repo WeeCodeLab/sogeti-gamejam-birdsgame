e5e4fa6e23ba12fb3980067a7fcc73ac57fe0b56
Олежа Блаженный <MrRavMorgan@gmail.com>
2022-06-12 17:55:08 +0300

    Update of GameUi
    Added SlideSwitchSound to PresentationUi

---
+++ birds-game/Assets/Scripts/Ui/GameUi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace birds_game.Assets.Scripts
+{
+    public class GameUi : MonoBehaviour
+    {
+        public HealthUi HealthUi;
+        public PresentationUi PresentationUi;
+
+        private void Start()
+        {
+            PresentationUi.gameObject.SetActive(false);
+        }
+    }
+}
